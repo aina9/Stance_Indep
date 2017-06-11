3766548036813ec6c37d1e9e47ad60ff5fae8fee
Mike Rosner <hoavutrongvn@gmail.com>
2017-06-11 18:55:26 +0200

    virtualenv

--- src/nb_word_bigrams.py
+++ src/nb_word_bigrams.py
@@ -5,7 +5,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 
-X = [] ; y = []
+X, y, c = [], [], [] 
 bigram_vectorizer = CountVectorizer(ngram_range=(1, 2),
                                     token_pattern=r'\b\w+\b',
                                     stop_words=['URL'])
@@ -15,15 +15,22 @@ clf = MultinomialNB()
 with open("../data/full.txt") as f:
     for line in f:
         cols = line.split("\t")
-        # tweet, class
-        X.append(cols[1]) ; y.append(cols[4])
+        # tweet, class, lang
+        X.append(cols[1]) ; y.append(cols[4]) ; c.append(cols[5].rstrip("\n"))
         
 X = bigram_vectorizer.fit_transform(X).toarray()
 print(y[:10])
 y = encoder.fit_transform(y)
 print(y[:10])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_mid, y_train, y_mid, c_train, c_test = train_test_split(X, y, c)
+X_test, y_test = [], []
+
+for X, y, c in zip(X_mid, y_mid, c_test):
+    if c == 'cat':
+        X_test.append(X)
+        y_test.append(y)
+
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
